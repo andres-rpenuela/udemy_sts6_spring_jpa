b335f7568c71a782a994b587104b90db2c75d2ff
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2025-08-20 16:47:09 +0200

    [sts6] se crea un projecto para la gestion de relaciones en la entidad

---
+++ demospring_boot3_jpa_relationship/src/main/java/com/codearp/application/demospring_boot3_jpa_relationship/SpringBoot3JpaRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.codearp.application.demospring_boot3_jpa_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3JpaRelationshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3JpaRelationshipApplication.class, args);
+    }
+
+}
---
+++ demospring_boot3_jpa_relationship/src/test/java/com/codearp/application/demospring_boot3_jpa_relationship/SpringBoot3JpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codearp.application.demospring_boot3_jpa_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3JpaRelationshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
